b8da5c90d5532221b4c5c3d64d6c7b5e9d559a62
linyu <972223177@qq.com>
2021-10-26 13:39:38 +0800

    [feature]utils

---
+++ app/src/main/java/com/ly/chatcompose/utils/SafePostKtx.kt
@@ -0,0 +1,59 @@
+package com.ly.chatcompose.utils
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.*
+
+/**
+ * 代替view.post的形式，具体原理可以看协程源码关于Android中Dispatcher.Main的具体实现HandlerContext，断点时注意当前Coroutine版本
+ * 在ComponentActivity中直接 safePost{ }
+ * 在Fragment中：
+ *              1.safePost{ }
+ *              2.viewLifecycleOwner.safePost{ } 使用getViewLifecycleOwner时如果不能保证生命周期安全
+ *              最好try-catch，此方法在viewLifecycleOwner为空时会抛异常
+ * @param postWhen post事件的具体时机
+ * @param block 你的runnable的具体实现
+ */
+inline fun LifecycleOwner.safePost(postWhen: Lifecycle.State? = null, crossinline block: () -> Unit) {
+    postWhen.runBlock(this, 0L, block)
+}
+
+/**
+ * 同上，多个delay
+ */
+inline fun LifecycleOwner.safePostDelay(
+    millis: Long,
+    postWhen: Lifecycle.State? = null,
+    crossinline block: () -> Unit
+) {
+    postWhen.runBlock(this, millis, block)
+}
+
+inline fun Lifecycle.State?.runBlock(lifecycleOwner: LifecycleOwner, millis: Long, crossinline block: () -> Unit) {
+    with(lifecycleOwner) {
+        when (this@runBlock) {
+            Lifecycle.State.DESTROYED -> {
+            }
+            Lifecycle.State.INITIALIZED -> {
+            }
+            Lifecycle.State.CREATED -> lifecycleScope.launchWhenCreated {
+                launchOnMain(millis, block)
+            }
+            Lifecycle.State.STARTED -> lifecycleScope.launchWhenStarted { launchOnMain(millis, block) }
+            Lifecycle.State.RESUMED -> lifecycleScope.launchWhenResumed { launchOnMain(millis, block) }
+            else -> lifecycleScope.launch { launchOnMain(millis, block) }
+        }
+    }
+}
+
+inline fun CoroutineScope.launchOnMain(millis: Long = 0L, crossinline block: () -> Unit) {
+    launch(Dispatchers.Main) {
+        if (millis > 0L) {
+            delay(millis)
+        }
+        if (isActive) {
+            block()
+        }
+    }
+}
